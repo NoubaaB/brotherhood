3d0afcfebce5acc75979a1e3dce256e307a60067
NoubaaB <bachirnoubaa@hotmail.com>
2024-08-02 18:28:05 +0100

    update projet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,15 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/gif" href="/storage/atom.gif">
+    <!-- <link rel="icon" type="image/gif" href="/storage/atom.gif"> -->
+    <link rel="icon" type="image/png" href="/storage/icons/icon-144x144.png" sizes="144x144" async defer>
+    <link rel="manifest" href="/manifest.json">
+    <link rel="apple-touch-icon" href="/storage/icons/icon-144x144.png" sizes="144x144">
+    <meta name="theme-color" content="#fff" />
+    <meta name="background_color" content="#fff" />
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Brotherhood</title>
+    <title>Brotherhood App</title>
     @vite(['resources/css/app.css'])
 
 </head>
