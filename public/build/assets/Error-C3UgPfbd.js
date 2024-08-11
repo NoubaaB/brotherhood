5abce7c3806d1446e3b959f88c7f4c1d934b7a64
NoubaaB <bachirnoubaa@hotmail.com>
2024-08-11 16:56:24 +0100

    update project

---
+++ public/build/assets/Error-C3UgPfbd.js
@@ -0,0 +1,1 @@
+import{q as r}from"./app-BdDNodmY.js";const t=r("Error",{state:()=>({text:"",image:""})});export{t as u};
--- public/build/assets/Error-C685Ly0m.js
+++
@@ -1,1 +0,0 @@
-import{q as r}from"./app-DYex-Tyi.js";const t=r("Error",{state:()=>({text:"",image:""})});export{t as u};
