1df65d4f4f0da1b4e9fea8c8fd801579032f1a36
NoubaaB <bachirnoubaa@hotmail.com>
2024-09-18 13:30:39 +0100

    update project

---
+++ public/build/assets/Error--kg6Q-sJ.js
@@ -0,0 +1,1 @@
+import{s as r}from"./app-CgKV6O7-.js";const t=r("Error",{state:()=>({text:"",image:""})});export{t as u};
--- public/build/assets/Error-anpJTuYD.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./app-CT2O4J6V.js";const t=r("Error",{state:()=>({text:"",image:""})});export{t as u};
