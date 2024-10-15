47e9c4f0711a907289c290074e569d290826c5c1
NoubaaB <bachirnoubaa@hotmail.com>
2024-10-15 10:50:36 +0100

    update project

--- public/build/assets/Error-CdqQgYx9.js
+++
@@ -1,1 +0,0 @@
-import{v as e,i as t}from"./app-C7Wo4OOA.js";const r=e("Error",{state:()=>({text:"",image:"",text_deactivated:`Dear ${t().user.name}, Your Account Has been Deactivated 😥`,image_deactivated:"/deactivated.gif"})});export{r as u};
